e89cfada1ed520c6ea7bcbe1973cb26f3b4b23a3
mhmdrameez <muhammedrameez2000@gmail.com>
2026-03-18 10:27:38 +0530

    fix:change in text

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
               .
             </h1>
             <p className="max-w-2xl text-sm leading-6 text-zinc-600 dark:text-zinc-400">
-              Developer upload extension folder, system make it zip automatically, users can download directly and use.
+              Upload extension folder, system makes it ZIP, users download and use.
             </p>
           </div>
 
